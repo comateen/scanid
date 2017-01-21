d302fe1c872a3ecd2dbc0da2f3cbd7c14c5f86af
jof <comateen_1@hotmail.com>
2017-01-21 19:25:37 +0100

    .

--- src/scanid.java
+++ src/scanid.java
@@ -23,13 +23,13 @@ public class scanid {
 
             // Envoi la commande Select à l' Applet
             byte[] aid = {(byte)0xA0, 0x00, 0x00, 0x00, 0x62, 0x03, 0x01, 0x0C, 0x06, 0x01};
-            ResponseAPDU answer = channel.transmit(new CommandAPDU(0x00, 0xA4, 0x04, 0x00, aid));
-            System.out.println("answer: " + answer.toString());
+            ResponseAPDU reponse = channel.transmit(new CommandAPDU(0x00, 0xA4, 0x04, 0x00, aid));
+            System.out.println("reponse: " + reponse.toString());
 
             // Envoi la commande de test
-            answer = channel.transmit(new CommandAPDU(0x00, 0x00, 0x00, 0x00));
-            System.out.println("answer: " + answer.toString());
-            byte r[] = answer.getData();
+            reponse = channel.transmit(new CommandAPDU(0x00, 0x00, 0x00, 0x00));
+            System.out.println("reponse: " + reponse.toString());
+            byte r[] = reponse.getData();
             for (int i=0; i<r.length; i++)
                 System.out.print((char)r[i]);
             System.out.println();
@@ -37,7 +37,7 @@ public class scanid {
             // Deconnect la carte
             card.disconnect(false);
         } catch(Exception e) {
-            System.out.println("Y a un probleme mon chou, ta carte est pas connectée " + e.toString());
+            System.out.println("Y a un probleme mon chou " + e.toString());
         }
     }
 }
